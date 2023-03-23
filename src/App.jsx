8a15aa80940ed32c651ff7d3c27379653c77838a
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2023-03-23 22:25:20 +0100

    INIT

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
